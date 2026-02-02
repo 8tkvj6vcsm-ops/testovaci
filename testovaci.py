9fb0cc3d493db8fbd5964918172aeba5e77ff8e3
milX <1urban.miloslav@gmail.com>
2026-02-02 22:39:34 +0100

    Testovani ruzne pri lekcich vid

---
+++ testovaci.py
@@ -0,0 +1,19 @@
+import random
+
+moznosti = ['kamen', 'nuzky', 'papir']
+hrac_volby = list()
+pocitac_volby = list()
+
+while len(hrac_volby) < 3 or len(pocitac_volby) < 3:
+  if len(hrac_volby) < 3:
+    hrac_volby.append(random.choice(moznosti))
+  if len(pocitac_volby) < 3:
+    pocitac_volby.append(random.choice(moznosti))
+
+for i in range(3):
+  if hrac_volby[i] == pocitac_volby[i]:
+      vysledek = "Remíza!"
+  else: 
+      vysledek = "Vyhrává hráč!"
+  
+  print(f"Výsledek: {vysledek}")
