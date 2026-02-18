8168e7f3700f2312abb754e64c15930e5ddda5db
milX <1urban.miloslav@gmail.com>
2026-02-18 11:45:55 +0100

    pokracovani

---
+++ main.py
@@ -0,0 +1,5 @@
+import modul
+
+print("main.py běží")
+
+modul.pozdrav()
---
+++ modul.py
@@ -0,0 +1,11 @@
+print("Načítám modul.py")
+
+def pozdrav():
+    print("Ahoj z funkce pozdrav()")
+
+def test():
+    print("Toto je testovací funkce")
+
+if __name__ == "__main__":
+    print("modul.py běží jako hlavní program")
+    test()
