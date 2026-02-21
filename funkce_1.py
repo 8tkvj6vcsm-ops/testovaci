3822d195372f2545e0df81cb527f74001c851f59
Milo <122271531+n4bru@users.noreply.github.com>
2026-02-21 17:15:08 +0100

    dalsi ukol engeto

---
+++ funkce_1.py
@@ -0,0 +1,12 @@
+vstupni_text = "Ahoj všem, tady Engeto"
+
+def zdvojnasob_znaky(text):
+  pass
+
+zdvojene = list()
+
+for p in vstupni_text:
+  if p == "m":
+    zdvojene.append("m")
+    zdvojnasob_znaky(zdvojene)
+print(zdvojnasob_znaky)
